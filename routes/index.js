4b0d408e1d6a8323b4d16e280c70c697df8237ad
Rita <hsen.yeh123@gmail.com>
2021-05-31 21:21:02 +0800

    feat: add login/register page

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const expense = require('./modules/expense')
 const filter = require('./modules/filter')
+const users = require('./modules/users')
 
 
-router.use('/', home)
+router.use('/users', users)
 router.use('/expense', expense)
 router.use('/filter', filter)
+router.use('/', home)
 
 
 module.exports = router
