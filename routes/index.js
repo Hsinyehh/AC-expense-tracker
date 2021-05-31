00c812b03afb7dd7279393fc653a6518fd40ef6f
Rita <hsen.yeh123@gmail.com>
2021-06-01 00:00:23 +0800

    feat: add auth to routers

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,13 @@ const home = require('./modules/home')
 const expense = require('./modules/expense')
 const filter = require('./modules/filter')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
 
 router.use('/users', users)
-router.use('/expense', expense)
-router.use('/filter', filter)
-router.use('/', home)
+router.use('/expense', authenticator, expense)
+router.use('/filter', authenticator, filter)
+router.use('/', authenticator, home)
 
 
 module.exports = router
