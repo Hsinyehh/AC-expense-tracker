4ce225687e355277ef47f394ef13987368a9aae3
Rita <hsen.yeh123@gmail.com>
2021-06-07 00:10:20 +0800

    refact: combine filter.js and home.js together

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,45 +6,62 @@ const categoryList = require('../../models/seeds/category.json')
 
 
 router.get('/', async (req, res) => {
-
   const userId = req.user._id
-  const selectCategory = "All"
-  const selectMonth = "All"
-  let data
+  let selectCategory = req.query.filter
+  let selectMonth = req.query.filterByMon
   let totalAmount = 0
+  let data
+
+
+  //首頁篩選都要賦值為All
+  if (!selectCategory) {
+    selectCategory = "All"
+  }
+  if (!selectMonth) {
+    selectMonth = "All"
+  }
+
+
+  //設定篩選物件
+  let matchObject = { userId: userId }
+  if (selectCategory !== "All") {
+    matchObject = { ...matchObject, category: selectCategory }
+  }
+  if (selectMonth !== "All") {
+    matchObject = { ...matchObject, month: selectMonth }
+  }
 
   try {
     data = await Record.aggregate(
       [
-
-        { $match: { userId: userId } },
+        { $match: { ...matchObject } },
         {
           $group: {
-            _id: "null",
+            _id: 'null',
             total: {
               $sum: "$amount"
+
             }
           }
-        }
-      ])
+        }])
 
   }
-  catch (error) { console.error('home aggregate error') }
+  catch (error) { console.error('filter aggregate error') }
+  //如果篩選到資料，就賦值給totalAmount；沒篩選到資料，totalAmount=0
   if (!(data == 0)) {
     totalAmount = data[0].total
   }
 
-  Record.find({ userId })
+
+  return Record.find({
+    ...matchObject
+  })
     .lean()
-    .then(records =>
-      res.render('index', {
-        records: records, totalAmount: totalAmount, selectCategory: selectCategory, selectMonth: selectMonth, categoryList: categoryList
-      }))
-    .catch(error => console.error('home find error'))
+    .then(records => res.render('index', { records: records, totalAmount: totalAmount, selectCategory: selectCategory, selectMonth: selectMonth, categoryList: categoryList }))
+    .catch(error => console.error('filter find error'))
+
 
 })
-
-
 
 
 module.exports = router
